d596a21f57e4a9a2686d060bb15ec5452a72c048
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-25 15:48:52 -0500

    users model implented and hashed passwords

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const User = require('../models/user')
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
 
-router.post('/signup', (req,res) => {
-    console.log(req.body)
+router.post('/signup', async (req,res) => {
+    try {
+        
+        const {email, password } = req.body;
+        let hashedPwd = bcrypt.hashSync(password, 10);
+        const user =  User({email : email, password: hashedPwd});
+        await user.save();
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
     res.send(' You made it')
 })
 
